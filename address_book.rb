e010beedd05386f96a204b97f6f09f222dc75ec8
ThomasWhyyou <thomas.whyyou@ymail.com>
2013-06-27 22:43:15 -0400

    Finished setting up all pages. Ready for submision

--- address_book.rb
+++ address_book.rb
@@ -1,46 +1,52 @@
-# use this code to create an address book in sinatra
-# create an input form
-# add a person
-# list people
 require 'pg'
 require 'pry'
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader' if development?
 
-puts "Hey gurl"
-
-# get all the inputs
-# put them in the string
-# make it work
-
-# this establishes a connection to the database
 # db = PG.connect(:dbname => 'address_book',
 #   :host => 'localhost')
-# executing sql code
-# passing a string of sql to the database
-
-# insert into database
-db = PG.connect(:dbname => 'address_book',
-  :host => 'localhost')
-
-puts "what's your name girl?"
-name = gets.chomp
-sql = "insert into contacts (first) values ('#{name}')"
-db.exec(sql)
-sql = "select first, age from contacts"
-db.exec(sql) do |result|
-  result.each do |row|
-    puts row
-  end
-end
-# db.close
-db.close
-
-# reads from database
-# db = PG.connect(:dbname => 'address_book',
-#   :host => 'localhost')
-# sql = "select first, age from contacts"
+# sql = "select * from contacts"
 # db.exec(sql) do |result|
 #   result.each do |row|
 #     puts row
 #   end
 # end
 # db.close
+
+
+# METHOD TO ACCESS DATA BASE
+def access_db
+  db = PG.connect(
+    :dbname => 'address_book',
+    :host => 'localhost')
+  return db
+end
+
+get '/' do
+  @db_accessed = access_db
+  erb :getdata
+end
+
+post '/' do
+  @first = params[:first].chomp.capitalize
+  @last = params[:last].chomp.capitalize
+  @age = params[:age].chomp.to_i
+  @gender = params[:gender].chomp.upcase
+  @dtgd = params[:dtgd].chomp
+  @phone = params[:phone].chomp
+
+  @db_accessed = access_db
+  sql_input = "INSERT INTO contacts (first, last, age, gender, dtgd, phone) VALUES ('#{@first}', '#{@last}', '#{@age}', '#{@gender}', '#{@dtgd}', '#{@number}')"
+  @db_accessed.exec(sql_input)
+  @db_accessed.close
+
+  redirect to("/thanks")
+end
+
+get '/thanks' do
+  erb :thankyou
+end
+
+
+
